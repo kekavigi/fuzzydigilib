88f7a10de5c3bb52be8f807f6d659ba85fa96040
Keyka Vigiliant <keka.vigi@gmail.com>
2020-12-13 20:45:32 +0700

    ganti metode searching

--- heroku/app.py
+++ heroku/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from pickle import load
-from rapidfuzz.fuzz import token_set_ratio as T
 
 ######################
 
@@ -10,16 +9,30 @@ with open('dt.pickle', 'rb') as handle:
     DICTI, TOTAL = load(handle)
 LENDICTI = len(DICTI)
 
+def found(word, textset):
+    l = len(word)
+    for text in textset:
+        if len(text)>=l and text[:l]==word:
+            return True
+    return False
+
+def founds(words, textset):
+    for word in words.lower().split():
+        if not found(word, textset):
+            return False
+    return True
+
 ######################
 
 @app.route('/', methods = ['GET', 'POST'])
 def search():
     if request.method == 'GET':
         return render_template('home.html')
+
     if request.method == 'POST':
         words = request.form['searchbar']
         result = [DICTI[i] for i, text in zip(range(LENDICTI), TOTAL)
-                if T(text, words)==100]
+                if founds(words, text)]
 
         return render_template('home.html',
                             in_search_mode = True,
