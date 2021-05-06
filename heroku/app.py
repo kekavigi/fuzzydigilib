5f57ad6bfd4bd2a17dfc547aaac27968ed6d7a01
Keyka Vigiliant <keka.vigi@gmail.com>
2021-05-06 16:25:35 +0700

    ubah db ke mongodb

--- heroku/app.py
+++ heroku/app.py
@@ -1,3 +1,6 @@
+import pymongo
+import os
+
 from flask import Flask, render_template, request
 from pickle import load
 
@@ -5,30 +8,9 @@ from pickle import load
 
 app = Flask(__name__)
 
-with open('d.pickle', 'rb') as handle:
-    DICTI = load(handle)
-    
-LENDICTI = len(DICTI)
-
-def transform(row):
-    res = set()
-    for col in row:
-        res.update(col.lower().split())
-    return res
-
-def found(word, textset):
-    l = len(word)
-    for text in textset:
-        if len(text)>=l and text[:l]==word:
-            return True
-    return False
-
-def founds(words, text):
-    textset = transform(text)
-    for word in words.lower().split():
-        if not found(word, textset):
-            return False
-    return True
+url = os.environ.get('MONGODB_URI', None)
+client = pymongo.MongoClient(url)
+katalog = client.digilib.katalog
 
 ######################
 
@@ -39,13 +21,19 @@ def search():
 
     if request.method == 'POST':
         words = request.form['searchbar']
-        result = [DICTI[i] for i, text in zip(range(LENDICTI), DICTI)
-                if founds(words, text)]
-
+        
+        # cari exact match di database
+        cursor = katalog.find({'$text': { '$search': '\"{}\"'.format(words) }},
+                              {'_id':0, 'id':0})
+        
+        # simplify format to list of dict
+        result = [x for x in cursor]
+        
+        # render it on screen
         return render_template('home.html',
                             in_search_mode = True,
                             query = words,
-                            data = result,
+                            result = result,
                             datasize = len(result))
 
 if __name__ == '__main__':
